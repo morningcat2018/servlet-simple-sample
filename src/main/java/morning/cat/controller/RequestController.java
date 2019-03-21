e96ffaf16ce135bdb6ea710cddcf8ecc2cfe453e
morningcat <mengzhang1993@gmail.com>
2019-03-22 01:19:03 +0800

    simple

--- src/main/java/morning/cat/controller/RequestController.java
+++ src/main/java/morning/cat/controller/RequestController.java
@@ -6,19 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.annotation.RequestScope;
 
-/**
- * @describe: 类描述信息
- * @author: morningcat.zhang
- * @date: 2019/3/20 10:13 PM
- */
 @Controller
-@Scope("request")
+//@Scope("request")
 public class RequestController {
 
     /**
      * http://localhost:8080/request
-     *
-     * @return
      */
     @RequestMapping("/request")
     @ResponseBody
